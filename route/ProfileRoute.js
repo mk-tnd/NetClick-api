6fc87a068a37ebe7119317adb615be9cf2ea18e6
Palm <xelticross@gmail.com>
2021-05-17 18:24:55 +0700

    reset_branch_main2

--- route/ProfileRoute.js
+++ route/ProfileRoute.js
@@ -1,9 +1,5 @@
 const express = require('express')
 const router = express.Router()
-const ProfileControl = require('../controller/ProfileController')
-const protect = require('../middleware/passport')
-const upload = require('../middleware/upload')
 
-router.post('/newProfile', protect, upload.single('image'), ProfileControl.createProfile)
-router.put('/editProfile/:profileId', protect, upload.single('image'), ProfileControl.editProfile)
+
 module.exports = router
--- route/UserRoute.js
+++ route/UserRoute.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const userControl = require('../controller/UserController')
 
-router.post('/register', userControl.register)
-router.post('/login', userControl.login)
+
 
 module.exports = router

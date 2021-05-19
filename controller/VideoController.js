15db5a709764b1977e4a5c893d76d2e32a60edc3
Mickie <mickey_saechia@hotmail.com>
2021-05-20 00:16:54 +0700

    update create video

--- controller/VideoController.js
+++ controller/VideoController.js
@@ -1,33 +1,16 @@
 const { videos } = require("../models");
-const cloudinary = require("cloudinary").v2;
-const fs = require("fs");
 
 exports.createVideo = async (req, res, next) => {
   try {
     const { name, vname, description, status, categoryId } = req.body;
-    if (req.file) {
-      cloudinary.uploader.upload(req.file.path, async (err, result) => {
-        if (err) return next(err);
-        const video = await videos.create({
-          name,
-          vname: result.secure_url || null,
-          description,
-          status,
-          categoryId,
-        });
-        fs.unlinkSync(req.file.path);
-        res.status(200).json({ video });
-      });
-    } else {
-      const video = await videos.create({
-        name,
-        vname: null,
-        description,
-        status,
-        categoryId,
-      });
-      res.status(200).json({ video });
-    }
+    const video = await videos.create({
+      name,
+      vname,
+      description,
+      status: "Showing",
+      categoryId,
+    });
+    res.status(200).json({ video });
   } catch (err) {
     next(err);
   }

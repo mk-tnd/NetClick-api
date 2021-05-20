e07dc9b3df28e6768e9492a491786d445fb94797
Palm <xelticross@gmail>
2021-05-20 19:30:13 +0700

    completed package,and category ,and adjust video controller

--- route/Category.js
+++ route/Category.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const CategoryControl = require('../controller/CategoryController')
+const protect = require('../middleware/passport')
 
-router.post('/register', CategoryControl.createCategory)
-router.put('/edit/:id', CategoryControl.editCategory)
-
+router.post('/register', protect, CategoryControl.createCategory)
+router.put('/edit/:id', protect, CategoryControl.editCategory)
+router.get('/', protect, CategoryControl.getCategory)
 module.exports = router
